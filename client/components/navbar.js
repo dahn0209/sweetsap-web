d8ceaaad73ce64c0d2eb00ad92f5a481c0171009
dahn0209 <dahn0209@gmail.com>
2021-11-03 01:52:58 -0400

    added image to index.html

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1 id="description">Restaurant and Bar</h1>
+    <h1 id="description">Restaurant & Bar</h1>
     <nav>
       {isLoggedIn ? (
         <div>
